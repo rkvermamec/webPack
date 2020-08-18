c98916de63225b962f3347a8bd9f7449100c0482
Ravi Kant Verma <rkvermamec@gmail.com>
2020-08-18 14:42:57 +0530

    removed

--- docs/assets/labels/labels_hi.ts
+++
@@ -1,8 +0,0 @@
-export const labels_hi = {
-    'errorMobile': 'Invalid mobile number.',
-    'errorOtp': 'Invalid OTP',
-    'errorName1': 'Name is mandatory',
-    'errorName2': 'Name length can\'t be greater than 30 charecters',
-    'errorPassword1': 'Password length can\'t be than 6 charecters',
-    'errorPassword2': 'Password and Confirm Password not matched'
-} 
